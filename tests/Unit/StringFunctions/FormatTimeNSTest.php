e06a657fdfd262627a6d59247e52292934ceeaee
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-09-13 16:39:55 +0300

    ns

--- tests/Unit/StringFunctions/FormatTimeNSTest.php
+++ tests/Unit/StringFunctions/FormatTimeNSTest.php
@@ -3,6 +3,7 @@
 namespace AlecRabbit\Tests\Helpers;
 
 use function AlecRabbit\format_time_ns;
+use const AlecRabbit\Helpers\Constants\PHP_ARCH;
 
 class FormatTimeNSTest extends HelpersTestCase
 {
@@ -19,9 +20,15 @@ class FormatTimeNSTest extends HelpersTestCase
 
     public function functionFormatTimeNSDataProvider(): array
     {
-        return [
+        if (64 === PHP_ARCH) {
+            $arr[] = ['2.806h', 10100000000000];
+        } elseif (32 === PHP_ARCH) {
+            //
+        } else {
+            throw new \RuntimeException('Unknown architecture nor 64bit nor 32bit.');
+        }
+        $arr = [
             // [$expected, $value],
-            ['2.806h', 10100000000000],
             ['1.0ms', 1010000],
             ['1.2s', 1200000000],
             ['1.0s', 1000000000],
@@ -31,5 +38,6 @@ class FormatTimeNSTest extends HelpersTestCase
             ['1.0μs', 1000,],
             ['10.0ns', 10,],
         ];
+        return $arr;
     }
 }
